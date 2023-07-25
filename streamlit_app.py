2084aa0b884a628642afe1b181fa104c19ee6eeb
pavangampa97 <69684031+pavangampa97@users.noreply.github.com>
2023-07-25 22:42:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
 streamlit.title('My Mom's New Healthy Dinner')
+
 streamlit.header('Breakfast Favourites')
 streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text( '🥗 Kale, Spinach & Rocket Smoothie ')
